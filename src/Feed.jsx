6208b20fea0c33a5c1ee263bfe786b0aa9e00e94
heitzuli <heidi.poels@gmail.com>
2024-01-14 15:14:37 +0200

    Add delete button

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -3,9 +3,15 @@ import {useState} from "react";
 export function Feed() {
     const [post, setPost] = useState("");
     const [posts, setPosts] = useState([]);
-    function activateLasers () {
+    function activateLasers (event) {
+        event.preventDefault();
         setPosts([...posts, post]);
         setPost("");
+    }
+    function deactivateLasers (index) {
+        const newPosts = [...posts];
+        newPosts.splice(index, 1);
+        setPosts(newPosts);
     }
     return (
         <div>
@@ -15,14 +21,19 @@ export function Feed() {
                       onChange={(event) => {
                           setPost(event.target.value);
                       }}></textarea>
-            <button onClick={activateLasers}>Tell people!</button>
-            <div>
-                {posts.map((post) => (
-                    <div>
+            <button onClick={activateLasers}>Tell me everything</button>
+            <ul>
+                {posts.map((post, index) => (
+                    <li key={index}>
                         {post}
-                    </div>
+                        <button className="delete-button" onClick={()=> deactivateLasers(index)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </button>
+                    </li>
                 ))}
-            </div>
+            </ul>
         </div>
 
     )
